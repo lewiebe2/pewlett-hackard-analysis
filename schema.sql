9677e89c7131bbdb7904acf6a029f0bf7af018b8
Lauren Wiebe <lewiebe2@gmail.com>
2021-12-06 20:47:59 -0800

    Adding .sql files and retirement_info.csv file.

--- schema.sql
+++ schema.sql
@@ -44,16 +44,14 @@ CREATE TABLE dept_emp (
   dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Creating tables for PH-EmployeeDB; Title Table.
-CREATE TABLE title (
+CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
